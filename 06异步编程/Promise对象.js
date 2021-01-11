f67e2a09431627cb7218cb045cc5039a00f15f29
sam <2324831337@qq.com>
2021-01-11 21:13:55 +0800

    完成了 异步以及 Promise 章节的再再再复习

--- 06异步编程/Promise对象.js
+++ 06异步编程/Promise对象.js
@@ -1,5 +1,7 @@
-// Promise 是 JavaScript 的异步操作解决方案, 为异步操作提供统一接口
-// 避免回调地狱, 使得异步操作具有同步操作的接口(向写同步代码一样写异步代码)
+/*
+  Promise 是 JavaScript 的异步操作解决方案, 为异步操作提供统一接口
+  避免了回调地狱, 使得异步操作具有同步操作的接口(像写同步代码一样写异步代码)
+*/
 /*
   Promise 的设计思想是: 所有的异步任务都返回一个 Promise 实例, Promise 实例有一个 then 方法, 用来指定下一步的回调函数
   多层嵌套 => 链式操作
@@ -13,74 +15,108 @@
     pending => rejected 未完成到失败 传回一个 error
 
     一旦状态发生改变, 凝固, 不会再有新的状态变化(Promise 实例的状态变化只能发生一次)
+    一次性, 凝固, 不会再改变
 */
 
 /*
   Promise 构造函数, 构造 Promise 的实例对象
 */
+var promise = new Promise((resolve, reject) => {
+  if (true) {
+    resolve(value); /* 异步操作成功, 将结果作为 resolve 函数的参数传递出去 */
+  } else {
+    reject(
+      new Error()
+    ); /* 异步操作失败, 将异步操作报出的错误, 作为参数传递出去 */
+  }
+});
 
 // Promise.prototype.then() 添加回调函数(两个)
+var p1 = new Promise((resolve, reject) => {
+  resolve('成功');
+});
+p1.then(console.log, console.error);
+
+var p2 = new Promise((resolve, reject) => {
+  reject(new Error('失败!'));
+});
+p2.then(console.log, console.error);
+
 /*
   Promise 对象的报错具有传递性
 */
 
 // 写法一
 f1().then(function () {
-  return f2()
-})
+  return f2();
+});
 
 // 写法二
 f1().then(function () {
-  f2()
-})
+  f2();
+  return;
+});
 
 // 写法三
-f1().then(f2())
+f1().then(f2());
 
 // 写法四
-f1().then(f2)
+f1().then(f2);
 
 // 解析
 // 写法一
 f1()
   .then(function () {
-    return f2()
+    return f2();
   })
-  .then(f3) // f3 的回调函数的参数是 f2() 函数的运行结果
+  .then(f3); // f3 的回调函数的参数是 f2() 函数的运行结果
 
 // 写法二
 f1()
   .then(function () {
-    f2()
-    return // undefined
+    f2();
+    return; // undefined
   })
-  .then(f3) // f3 的回调函数的参数是 undefined
+  .then(f3); // f3 的回调函数的参数是 undefined
 
-// 写法三
-f1().then(f2()).then(f3) // f3 的回调函数的参数是 f2 函数返回的函数的(f2 函数的返回值是一个函数, 这个函数的运行结果传递给了 f3, 相当于执行了两个函数 f2()())运行结果
+// 写法三 f2() 的返回值是一个函数
+f1().then(f2()).then(f3); // f3 的回调函数的参数是 f2 函数返回的函数的(f2 函数的返回值是一个函数, 这个函数的运行结果传递给了 f3, 相当于执行了两个函数 f2()())运行结果
+
 // 写法四
 
-f1().then(f2).then(f3) // 和写法一只有一个差别, 就是 f2 会接收到 f1() 返回的结果
+f1().then(f2).then(f3); // 和写法一只有一个差别, 就是 f2 会接收到 f1() 返回的结果
+
+/*
+  Promise 的优点:
+  回调地狱 => 链式调用, 程序流程可以看得很清楚
+  状态一旦改变, 无论何时查询, 都能得到这个状态 "凝固",
+    这意味着, 无论何时为 Promise 实例添加回调函数, 该函数都能正确执行
+    不用担心是否错过了某个事件或信号
+    如果是传统写法, 通过监听事件来执行回调函数, 一旦错过了事件, 再添加回调函数是不会执行的
+
+  Promise 的缺点:
+  代码的可阅读性较差, 不能一眼看出功能, 一堆 then, 自己必须在 then 的回调函数里面理清逻辑
+*/
 
 /*
   Promise 的回调函数属于异步任务, 会在同步任务之后执行
   属于 "微任务"
   正常的异步任务追加到下一轮事件循环, "微任务" 追加到本轮事件循环
-  这意味着, "微任务" 的执行时间一定早于正常任务
+  这意味着, "微任务" 的执行时间一定早于正常的异步任务任务
 */
 setTimeout(() => {
-  console.log(1)
-}, 0)
+  console.log(1);
+}, 0);
 
 new Promise(function (resolve, reject) {
-  resolve(2)
-}).then(console.log)
+  resolve(2);
+}).then(console.log);
 
-console.log(3)
+console.log(3);
 /*
-  3
-  2
-  1
+  3 同步任务最先输出
+  2 Promise 的回调函数不是正常的异步任务而是微任务, 在本轮事件循环的末尾执行, 但是早于正常的异步任务
+  1 setTimeout 作为正常的异步任务, 被添加到下一轮事件循环, 晚于微任务
 
   then 是在本轮事件循环执行(末), setTimeout(fn, 0) 在下一轮事件循环开始时执行
 */
--- 06异步编程/定时器.js
+++ 06异步编程/定时器.js
@@ -1,5 +1,12 @@
 // 向任务队列添加定时任务, 也就是添加异步任务
-setTimeout(() => {}, timeout) // 指定某段代码, 某个函数, 在多少毫秒之后执行, 返回一个整数, 表示定时器的编号, 可以用来取消该定时器
+/*
+  指定某段代码, 某个函数, 在多少毫秒之后执行
+  第一个参数: 将要推迟执行的函数名, 或者一段代码(箭头函数等)
+  第二个参数: 推迟执行的毫秒数
+  更多的参数: 依次传入推迟执行的函数中
+  返回值: 一个整数, 表示定时器的编号(根据此编号可以取消定时器)
+*/
+setTimeout(() => {}, timeout)
 
 setInterval(() => {}, interval)
 
@@ -8,6 +15,7 @@ setInterval(() => {}, interval)
   而不是定义时所在的那个对象
 */
 var x = 1
+
 var obj = {
   x: 2,
   y: function () {
@@ -17,11 +25,12 @@ var obj = {
 // 由于 obj.y 在 1000 毫秒后执行, this 所指向的已经不是 obj 而是全局环境了
 setTimeout(obj.y, 1000) // 1 this 指向 window 而非 obj
 // 解决方案一: 将 obj.y 放入一个函数
-setTimeout(() => {
-  obj.y()
-}, 1000) // 2
 
-// 解决方案二: 使用 bind 方法, 将 obj.y 绑定在 obj 上面
+setTimeout(function()  {
+  obj.y()
+}, 1000); // 2
+
+// 解决方案二: 使用 bind 方法(返回一个新的函数(绑定了 this 的指向)), 将 obj.y 绑定在 obj 上面
 setTimeout(obj.y.bind(obj), 1000) // 2
 
 // setInterval 与 setTimeout 的用法完全一致, 区别仅仅在于 setInterval 指定某个任务每隔一段时间就执行一次, 无限次执行
@@ -29,6 +38,7 @@ var i = 1
 var timer = setInterval(() => {
   console.log(2)
 }, 1000)
+
 // setInterval 常用于实现轮询
 var hash = window.location.hash
 var hashWatcher = setInterval(() => {
@@ -37,6 +47,7 @@ var hashWatcher = setInterval(() => {
   }
 }, 1000)
 /*
+  setInterval 指的是 "开始执行" 之间的间隔
   setInterval 不考虑每次任务执行本身所消耗的时间, 因此, 实际上, 两次执行之间的间隔会小于指定的时间
   为了确保两次执行之间有固定的间隔, 使用 setTimeout (递归调用自身)
 */
@@ -46,6 +57,34 @@ var timer = setTimeout(function f() {
   // 不断递归调用自身实现 setInterval 的效果
   timer = setTimeout(f, 2000)
 }, 2000)
+
+/**
+ * setTimeout 以及 setInterval函数, 都返回一个整数值, 表示计数器编号
+ * clearTimeout 以及 clearInterval 函数传入计数器编号后, 就可以取消对应的定时器
+ */
+var id1 = setTimeout(f, 1000);
+var id2 = setInterval(f, 1000);
+clearInterval(id1)
+clearInterval(id2)
+
+/*
+  setTimeout 以及 setInterval 返回的整数是连续的, 第二个 setTimeout 方法返回的整数值, 将比第一个整数值大 1
+*/
+(function() {
+  // 每轮事件循环检查一次
+  var gid = setInterval(clearAllTimeouts, 0);
+  // console.log(gid);
+  function clearAllTimeouts() {
+      // 得到最大的定时器的序号 id
+    var id = setTimeout(function() {}, 0);
+    while (id > 0) {
+      if (id !== gid) {
+        clearTimeout(id);
+      }
+      id--;
+    }
+  }
+})();
 
 /*
   实例: debounce 函数
@@ -75,7 +114,8 @@ function debounce(fn ,delay) {
 */
 
 /*
-  setTimeout(f, 0)
+  setTimeout(f, 0) 会在下一轮事件循环一开始就执行
+  目的是, 尽可能早地执行 f 但是并不能保证立刻就执行 f
   可以调整事件的发生顺序,
 */
 <input type="button" id="myButton" value="click" />
